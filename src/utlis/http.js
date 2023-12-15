d196aabb0ba6141060780691eda4a31b8cadd32c
Dev Gupta <dev2000.gupta@gmail.com>
2023-12-16 00:14:39 +0530

    env issue fixed

--- src/utlis/http.js
+++ src/utlis/http.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const http = () => {
   const auth = JSON.parse(localStorage.getItem("auth"));
-  const BASE_URL = process.env.BASE_URL;
-  const DEPLOYED_URL = process.env.DEPLOYED_URL;
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const DEPLOYED_URL = process.env.REACT_APP_DEPLOYED_URL;
 
   return axios.create({
-    baseURL: window.location.host == "localhost:3000" ? BASE_URL : DEPLOYED_URL,
+    baseURL: window.location.host != "localhost:3000" ? BASE_URL : DEPLOYED_URL,
     headers: {
       "auth-token": auth?.authToken,
       "Content-Type": "application/json",
